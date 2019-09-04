a7a828364094ebc0c0c1da608b1691f4161edffc
Fredy Mosquera Lemus <fmosquera0101@gmail.com>
2019-09-03 23:01:50 -0500

    LinkedList implementation, insertFron, insertEnd and insertBetween

---
+++ DataStructure/src/co/com/fredymosquera/linkedlist/LinkedList.java
@@ -0,0 +1,40 @@
+package co.com.fredymosquera.linkedlist;
+
+/**
+ * 
+ * @author fredy mosqueralemus
+ *
+ */
+public class LinkedList {
+	
+	private Node head;
+	
+	public void insertAtFront(Object data) {
+		Node newNode = new Node(data);
+		newNode.setNext(head);
+		head = newNode;
+	}
+	
+	public void insertAtEnd(Object data) {
+		Node newNode = new Node(data);
+		if(head == null) {
+			head = newNode;
+		}else {
+			Node tail = head;
+			while(tail != null) {
+				tail = tail.getNext();
+			}
+			tail.setNext(newNode);
+			newNode.setNext(null);
+		}
+	}
+	public void insertBetween(Node prevNode, Object data) {
+		if(prevNode == null) {
+			throw new IllegalArgumentException("Argument prevNode can't be null");
+		}
+		Node newNode = new Node(data);
+		newNode.setNext(prevNode.getNext());
+		prevNode.setNext(newNode);
+	}
+	
+}
---
+++ DataStructure/src/co/com/fredymosquera/linkedlist/Node.java
@@ -0,0 +1,33 @@
+package co.com.fredymosquera.linkedlist;
+/**
+ * 
+ * @author fredy mosqueralemus
+ *
+ */
+public class Node {
+
+	private Node next;
+	private Object data;
+	
+	public Node(Object data) {
+		this.data = data;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+	
+	
+}
